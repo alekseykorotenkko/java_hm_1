7e4820324c0053d50e5758f6fabf5d359e1d641c
aleksey korotenko <aqvateg@ya.ru>
2016-11-05 13:43:24 +0200

    first commit

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,10 @@ public class Main {
         int c = 14 ;
         int d = 128 ;
 
-        System.out.println("x mod 10 =" + a % 10);
-        System.out.println("x mod 10 =" + b % 10);
-        System.out.println("x mod 10 =" + c % 10);
-        System.out.println("x mod 10 =" + d % 10);
+        System.out.println("x mod 2 =" + a % 2);
+        System.out.println("x mod 2 =" + b % 2);
+        System.out.println("x mod 2 =" + c % 2);
+        System.out.println("x mod 2 =" + d % 2);
 
 
     }
