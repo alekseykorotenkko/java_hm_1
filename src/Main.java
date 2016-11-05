d38ee2981ab3b45248f961fa56c846d19f170a97
aleksey korotenko <aqvateg@ya.ru>
2016-11-05 13:28:28 +0200

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
-        int a = 15 - 1;
-        int b = -a + 48;
-        int c = 14 +b;
-        int d = 128 - c;
+        int a = 15 ;
+        int b = 12;
+        int c = 14 ;
+        int d = 128 ;
 
-        int e = (a <= b +d) ? 0 : (a/b);
-        int f = (c > d - b - a) ? 0 : (d/c+b);
+        System.out.println("x mod 10 =" + a % 10);
+        System.out.println("x mod 10 =" + b % 10);
+        System.out.println("x mod 10 =" + c % 10);
+        System.out.println("x mod 10 =" + d % 10);
 
-        System.out.println("a=" +a);
-        System.out.println("b=" +b);
-        System.out.println("c=" +c);
-        System.out.println("d=" +d);
-        System.out.println("a/b=" +e);
-        System.out.println("d/c+b =" +f);
 
     }
 }
