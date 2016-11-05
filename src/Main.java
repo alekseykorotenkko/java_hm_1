da7c5e2b31b2fe6c8a12299e4f99e45117147407
aleksey korotenko <aqvateg@ya.ru>
2016-11-05 13:56:30 +0200

    first commit

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,12 @@ public class Main {
 
     public static void main(String[] args) {
         int a = 15 ;
-        int b = 12;
-        int c = 14 ;
-        int d = 128 ;
+        int b = a%2;
 
-        System.out.println("x mod 2 =" + a % 2);
-        System.out.println("x mod 2 =" + b % 2);
-        System.out.println("x mod 2 =" + c % 2);
-        System.out.println("x mod 2 =" + d % 2);
+        String c = (b == 0)? "Yes" : "No";
+
+        System.out.println(c);
+
 
 
     }
